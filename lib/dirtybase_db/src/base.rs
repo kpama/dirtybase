6b1ad93436e3539fb7c9a06ef07686fd9a3b9dd1
Jibao Mansaray <jibaomansaray@gmail.com>
2023-03-18 03:07:34 +0000

    DB: Add function that generates foriegn column for table name

--- lib/dirtybase_db/src/base.rs
+++ lib/dirtybase_db/src/base.rs
@@ -12,3 +12,20 @@ pub mod schema;
 pub mod table;
 pub mod user_table;
 pub mod where_join_operators;
+
+/// Transform a table name to a column name.
+/// The table name is appended with `_id`
+pub fn to_fk_column(foreign_table: &str) -> String {
+    format!("{}_id", foreign_table.to_ascii_lowercase())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_that_foreign_keys_ends_with_id() {
+        let name = to_fk_column("foo");
+        assert_eq!(name, "foo_id");
+    }
+}
