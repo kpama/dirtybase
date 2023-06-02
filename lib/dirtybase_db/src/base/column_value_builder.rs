d05b0de46efd76b79f5cd14f6d6a4268476a5dee
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-02 06:37:12 +0000

    Add a `Column/Value` builder

---
+++ lib/dirtybase_db/src/base/column_value_builder.rs
@@ -0,0 +1,41 @@
+use std::cell::RefCell;
+
+use super::{field_values::FieldValue, types::ColumnAndValue};
+
+pub struct ColumnAndValueBuilder {
+    data: RefCell<ColumnAndValue>,
+}
+
+impl ColumnAndValueBuilder {
+    pub fn new() -> Self {
+        Self {
+            data: RefCell::new(ColumnAndValue::new()),
+        }
+    }
+
+    /// Alias to `insert`
+    pub fn add<V: Into<FieldValue>>(&self, key: &str, value: V) -> &Self {
+        self.data.borrow_mut().insert(key.into(), value.into());
+        self
+    }
+
+    pub fn insert<V: Into<FieldValue>>(&self, key: &str, value: V) -> &Self {
+        self.data.borrow_mut().insert(key.into(), value.into());
+        self
+    }
+
+    /// Alias to `insert_field`
+    pub fn add_field<V: Into<FieldValue>>(&self, key: &str, value: FieldValue) -> &Self {
+        self.data.borrow_mut().insert(key.into(), value);
+        self
+    }
+
+    pub fn insert_field<V: Into<FieldValue>>(&self, key: &str, value: FieldValue) -> &Self {
+        self.data.borrow_mut().insert(key.into(), value);
+        self
+    }
+
+    pub fn build(self) -> ColumnAndValue {
+        self.data.into_inner()
+    }
+}
