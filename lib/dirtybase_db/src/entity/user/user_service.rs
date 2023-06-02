6f0d36364eeeca087509542bc5053614567b1993
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-02 11:38:26 +0000

    Remove refcell for now

--- lib/dirtybase_db/src/entity/user/user_service.rs
+++ lib/dirtybase_db/src/entity/user/user_service.rs
@@ -1,33 +1,32 @@
-use std::cell::RefCell;
-
 use super::{
     hash_password, user_entity::UserUpdateEntity, user_repository::UserRepository, UserEntity,
 };
 
 pub struct UserService {
-    user_repo: RefCell<UserRepository>,
+    user_repo: UserRepository,
 }
 
 impl UserService {
     pub fn new(user_repo: UserRepository) -> Self {
-        Self {
-            user_repo: RefCell::new(user_repo),
-        }
+        Self { user_repo }
     }
 
-    pub fn user_repo(&self) -> &RefCell<UserRepository> {
+    pub fn user_repo(&self) -> &UserRepository {
         &self.user_repo
     }
 
+    pub fn user_repo_mut(&mut self) -> &mut UserRepository {
+        &mut self.user_repo
+    }
+
     pub async fn create_admin_user(
-        &self,
+        &mut self,
         username: &str,
         email: &str,
         raw_password: &str,
     ) -> Result<UserEntity, anyhow::Error> {
         if let Ok(user) = self
             .user_repo
-            .borrow_mut()
             .find_one_by_username_and_email(username, email)
             .await
         {
@@ -45,15 +44,15 @@ impl UserService {
     }
 
     pub async fn save(
-        &self,
+        &mut self,
         user: UserEntity,
         id: Option<&str>,
     ) -> Result<UserEntity, anyhow::Error> {
         let record: UserUpdateEntity = user.into();
         if let Some(id) = id {
-            self.user_repo.borrow_mut().update(id, record).await
+            self.user_repo.update(id, record).await
         } else {
-            self.user_repo.borrow_mut().create(record).await
+            self.user_repo.create(record).await
         }
     }
 }
