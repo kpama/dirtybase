b9d66ea45e3be467cb29d6de326f1b2b19fff7cd
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-02 16:23:49 +0000

    Dirtybase: Take ownership of self

--- lib/dirtybase_db/src/base/column_value_builder.rs
+++ lib/dirtybase_db/src/base/column_value_builder.rs
@@ -14,23 +14,30 @@ impl ColumnAndValueBuilder {
     }
 
     /// Alias to `insert`
-    pub fn add<V: Into<FieldValue>>(&self, key: &str, value: V) -> &Self {
+    pub fn add<V: Into<FieldValue>>(self, key: &str, value: V) -> Self {
         self.data.borrow_mut().insert(key.into(), value.into());
         self
     }
 
-    pub fn insert<V: Into<FieldValue>>(&self, key: &str, value: V) -> &Self {
+    pub fn insert<V: Into<FieldValue>>(self, key: &str, value: V) -> Self {
         self.data.borrow_mut().insert(key.into(), value.into());
+        self
+    }
+
+    pub fn try_to_insert<V: Into<FieldValue>>(self, key: &str, value: Option<V>) -> Self {
+        if let Some(value) = value {
+            return self.insert(key, value);
+        }
         self
     }
 
     /// Alias to `insert_field`
-    pub fn add_field<V: Into<FieldValue>>(&self, key: &str, value: FieldValue) -> &Self {
+    pub fn add_field<V: Into<FieldValue>>(self, key: &str, value: FieldValue) -> Self {
         self.data.borrow_mut().insert(key.into(), value);
         self
     }
 
-    pub fn insert_field<V: Into<FieldValue>>(&self, key: &str, value: FieldValue) -> &Self {
+    pub fn insert_field<V: Into<FieldValue>>(self, key: &str, value: FieldValue) -> Self {
         self.data.borrow_mut().insert(key.into(), value);
         self
     }
