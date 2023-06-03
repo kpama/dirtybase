a7b7300bd55db3716f0eee1170e193108a2d3db5
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-03 05:42:52 +0000

    Change all columns' name to constent

--- lib/dirtybase_db/src/entity/user.rs
+++ lib/dirtybase_db/src/entity/user.rs
@@ -1,4 +1,7 @@
-use crate::base::manager::Manager;
+use crate::base::{
+    manager::Manager,
+    table::{CREATED_AT_FIELD, DELETED_AT_FIELD, ID_FIELD, INTERNAL_ID_FIELD, UPDATED_AT_FIELD},
+};
 
 mod user_entity;
 mod user_helpers;
@@ -12,6 +15,18 @@ pub use user_service::UserService;
 
 // Core User table name
 pub static USER_TABLE: &str = "core_user";
+
+// Fields
+pub const USER_TABLE_USERNAME_FIELD: &str = "username";
+pub const USER_TABLE_EMAIL_FIELD: &str = "email";
+pub const USER_TABLE_PASSWORD_FIELD: &str = "password";
+pub const USER_TABLE_RESET_PASSWORD_FIELD: &str = "reset_password";
+pub const USER_TABLE_STATUS_FIELD: &str = "status";
+pub const USER_TABLE_INTERNAL_ID_FIELD: &str = INTERNAL_ID_FIELD;
+pub const USER_TABLE_ID_FIELD: &str = ID_FIELD;
+pub const USER_TABLE_CREATED_AT_FIELD: &str = CREATED_AT_FIELD;
+pub const USER_TABLE_UPDATED_AT_FIELD: &str = UPDATED_AT_FIELD;
+pub const USER_TABLE_DELETED_AT_FIELD: &str = DELETED_AT_FIELD;
 
 pub fn hash_password(raw: &str) -> String {
     bcrypt::hash(raw, 8).unwrap()
@@ -29,11 +44,17 @@ pub async fn setup_users_table(manager: &Manager) {
         manager
             .create_table_schema(USER_TABLE, |table| {
                 table.id_set();
-                table.string("username").set_is_unique(true);
-                table.string("email").set_is_unique(true);
-                table.string("password").set_is_nullable(true);
-                table.boolean("reset_password").set_default("0"); // A flag that indicates a password change is required
-                table.string("status").set_default_from(UserStatus::Pending);
+                table.string(USER_TABLE_USERNAME_FIELD).set_is_unique(true);
+                table.string(USER_TABLE_EMAIL_FIELD).set_is_unique(true);
+                table
+                    .string(USER_TABLE_PASSWORD_FIELD)
+                    .set_is_nullable(true);
+                table
+                    .boolean(USER_TABLE_RESET_PASSWORD_FIELD)
+                    .set_default_from(false); // A flag that indicates a password change is required
+                table
+                    .string(USER_TABLE_STATUS_FIELD)
+                    .set_default_from(UserStatus::Pending);
                 table.timestamps();
                 table.soft_deletable();
             })
