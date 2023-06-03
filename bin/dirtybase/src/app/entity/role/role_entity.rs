50c5c271de85437601b9ec576348555b97d975a4
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-03 10:40:55 +0000

    Dirtybase: Implement entity, repo and service for `role`

--- bin/dirtybase/src/app/entity/role/role_entity.rs
+++ bin/dirtybase/src/app/entity/role/role_entity.rs
@@ -1,5 +1,93 @@
+use super::{
+    ROLE_TABLE_APP_ID_FIELD, ROLE_TABLE_CREATED_AT_FIELD, ROLE_TABLE_CREATOR_FIELD,
+    ROLE_TABLE_DELETED_AT_FIELD, ROLE_TABLE_EDITOR_FIELD, ROLE_TABLE_ID_FIELD,
+    ROLE_TABLE_INTERNAL_ID_FIELD, ROLE_TABLE_NAME_FIELD, ROLE_TABLE_UPDATED_AT_FIELD,
+};
+use chrono::{DateTime, Utc};
+use dirtybase_db::base::{
+    field_values::FieldValue,
+    helper::generate_ulid,
+    types::{FromColumnAndValue, IntoColumnAndValue},
+    ColumnAndValueBuilder,
+};
+
+#[derive(Debug, Clone)]
 pub struct RoleEntity {
-    internal_id: Option<u64>,
-    id: String,
-    core_user_id: String,
+    pub internal_id: Option<u64>,
+    pub id: Option<String>,
+    pub name: Option<String>,
+    pub core_app_id: Option<String>,
+    pub creator_id: Option<String>,
+    pub editor_id: Option<String>,
+    pub created_at: Option<DateTime<Utc>>,
+    pub updated_at: Option<DateTime<Utc>>,
+    pub deleted_at: Option<DateTime<Utc>>,
+}
+
+impl Default for RoleEntity {
+    fn default() -> Self {
+        Self {
+            internal_id: None,
+            id: None,
+            name: None,
+            core_app_id: None,
+            creator_id: None,
+            editor_id: None,
+            created_at: None,
+            updated_at: None,
+            deleted_at: None,
+        }
+    }
+}
+
+impl RoleEntity {
+    pub fn new() -> Self {
+        Self {
+            id: Some(generate_ulid()),
+            ..Self::default()
+        }
+    }
+}
+
+impl FromColumnAndValue for RoleEntity {
+    fn from_column_value(column_and_value: dirtybase_db::base::types::ColumnAndValue) -> Self {
+        Self {
+            internal_id: FieldValue::from_ref_option_into(
+                column_and_value.get(ROLE_TABLE_INTERNAL_ID_FIELD),
+            ),
+            id: FieldValue::from_ref_option_into(column_and_value.get(ROLE_TABLE_ID_FIELD)),
+            name: FieldValue::from_ref_option_into(column_and_value.get(ROLE_TABLE_NAME_FIELD)),
+            core_app_id: FieldValue::from_ref_option_into(
+                column_and_value.get(ROLE_TABLE_APP_ID_FIELD),
+            ),
+            creator_id: FieldValue::from_ref_option_into(
+                column_and_value.get(ROLE_TABLE_CREATOR_FIELD),
+            ),
+            editor_id: FieldValue::from_ref_option_into(
+                column_and_value.get(ROLE_TABLE_EDITOR_FIELD),
+            ),
+            created_at: FieldValue::from_ref_option_into(
+                column_and_value.get(ROLE_TABLE_CREATED_AT_FIELD),
+            ),
+            updated_at: FieldValue::from_ref_option_into(
+                column_and_value.get(ROLE_TABLE_UPDATED_AT_FIELD),
+            ),
+            deleted_at: FieldValue::from_ref_option_into(
+                column_and_value.get(ROLE_TABLE_DELETED_AT_FIELD),
+            ),
+        }
+    }
+}
+
+impl IntoColumnAndValue for RoleEntity {
+    fn into_column_value(self) -> dirtybase_db::base::types::ColumnAndValue {
+        ColumnAndValueBuilder::new()
+            .try_to_insert(ROLE_TABLE_ID_FIELD, self.id)
+            .try_to_insert(ROLE_TABLE_NAME_FIELD, self.name)
+            .try_to_insert(ROLE_TABLE_APP_ID_FIELD, self.core_app_id)
+            .try_to_insert(ROLE_TABLE_CREATOR_FIELD, self.creator_id)
+            .try_to_insert(ROLE_TABLE_EDITOR_FIELD, self.editor_id)
+            .try_to_insert(ROLE_TABLE_DELETED_AT_FIELD, self.deleted_at)
+            .build()
+    }
 }
---
+++ bin/dirtybase/src/app/entity/role/role_repository.rs
@@ -0,0 +1,74 @@
+use super::{RoleEntity, ROLE_TABLE, ROLE_TABLE_DELETED_AT_FIELD, ROLE_TABLE_ID_FIELD};
+use dirtybase_db::base::{
+    field_values::FieldValue, manager::Manager, types::FromColumnAndValue,
+    types::IntoColumnAndValue,
+};
+
+pub struct RoleRepository {
+    manager: Manager,
+}
+
+impl RoleRepository {
+    pub fn new(manager: Manager) -> Self {
+        Self { manager }
+    }
+
+    pub fn manager(&self) -> &Manager {
+        &self.manager
+    }
+
+    pub fn manager_mut(&mut self) -> &mut Manager {
+        &mut self.manager
+    }
+
+    pub async fn find_by_id_with_trashed(&mut self, id: &str) -> Result<RoleEntity, anyhow::Error> {
+        self.find_by_id(id, true).await
+    }
+
+    pub async fn find_by_id(
+        &mut self,
+        id: &str,
+        with_trashed: bool,
+    ) -> Result<RoleEntity, anyhow::Error> {
+        match self
+            .manager_mut()
+            .select_from_table(ROLE_TABLE, |q| {
+                q.select_all().eq(ROLE_TABLE_ID_FIELD, id);
+                if !with_trashed {
+                    q.is_null(ROLE_TABLE_DELETED_AT_FIELD);
+                }
+            })
+            .fetch_one_as_field_value()
+            .await
+        {
+            Ok(result) => Ok(RoleEntity::from_column_value(result)),
+            Err(e) => Err(e),
+        }
+    }
+
+    pub async fn create(
+        &mut self,
+        record: impl IntoColumnAndValue,
+    ) -> Result<RoleEntity, anyhow::Error> {
+        let kv = record.into_column_value();
+        let id: String = FieldValue::from_ref_option_into(kv.get(ROLE_TABLE_ID_FIELD));
+
+        self.manager.insert_record(ROLE_TABLE, kv).await;
+        self.find_by_id(&id, false).await
+    }
+
+    pub async fn update(
+        &mut self,
+        id: &str,
+        record: impl IntoColumnAndValue,
+    ) -> Result<RoleEntity, anyhow::Error> {
+        let column_and_values = record.into_column_value();
+        self.manager
+            .save_record(ROLE_TABLE, column_and_values, |q| {
+                q.eq(ROLE_TABLE_ID_FIELD, id);
+            })
+            .await;
+
+        self.find_by_id(id, false).await
+    }
+}
