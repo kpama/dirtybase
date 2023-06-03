cb8727cd5e33bf14c4b9362b8e903f7513a7566b
Jibao Mansaray <jibaomansaray@gmail.com>
2023-06-03 10:52:24 +0000

    Dirtybase DB: Start work on filtering users by `deleted_at` state

--- lib/dirtybase_db/src/entity/user/user_repository.rs
+++ lib/dirtybase_db/src/entity/user/user_repository.rs
@@ -1,19 +1,28 @@
 #![allow(dead_code)]
 
-use super::{UserEntity, USER_TABLE, USER_TABLE_ID_FIELD};
+use super::{
+    UserEntity, USER_TABLE, USER_TABLE_DELETED_AT_FIELD, USER_TABLE_EMAIL_FIELD,
+    USER_TABLE_ID_FIELD, USER_TABLE_INTERNAL_ID_FIELD, USER_TABLE_USERNAME_FIELD,
+};
 use crate::base::{
     field_values::FieldValue,
     manager::Manager,
+    query::QueryBuilder,
+    schema::SchemaManagerTrait,
     types::{FromColumnAndValue, IntoColumnAndValue},
 };
 
 pub struct UserRepository {
     manager: Manager,
+    no_trashed: bool,
 }
 
 impl UserRepository {
     pub fn new(manager: Manager) -> Self {
-        Self { manager }
+        Self {
+            manager,
+            no_trashed: true,
+        }
     }
 
     pub fn manager(&self) -> &Manager {
@@ -24,11 +33,25 @@ impl UserRepository {
         &mut self.manager
     }
 
+    /// Included soft deleted records
+    pub fn with_trash(&mut self) -> &mut Self {
+        self.no_trashed = true;
+        self
+    }
+
+    /// Do not include soft deleted records
+    pub fn without_trash(&mut self) -> &mut Self {
+        self.no_trashed = false;
+        self
+    }
+
     pub async fn find_on_by_internal_id(&mut self, id: u64) -> Result<UserEntity, anyhow::Error> {
         match self
             .manager_mut()
             .select_from_table(USER_TABLE, |q| {
-                q.select_all().eq("internal_id", id);
+                q.select_all()
+                    .eq(USER_TABLE_INTERNAL_ID_FIELD, id)
+                    .and_is_null(USER_TABLE_DELETED_AT_FIELD);
             })
             .fetch_one_as_field_value()
             .await
@@ -42,7 +65,9 @@ impl UserRepository {
         match self
             .manager_mut()
             .select_from_table(USER_TABLE, |q| {
-                q.select_all().eq("id", id);
+                q.select_all()
+                    .eq(USER_TABLE_ID_FIELD, id)
+                    .and_is_null(USER_TABLE_DELETED_AT_FIELD);
             })
             .fetch_one_as_field_value()
             .await
@@ -60,7 +85,10 @@ impl UserRepository {
         match self
             .manager_mut()
             .select_from_table(USER_TABLE, |q| {
-                q.select_all().eq("username", username).eq("email", email);
+                q.select_all()
+                    .eq(USER_TABLE_USERNAME_FIELD, username)
+                    .eq(USER_TABLE_EMAIL_FIELD, email)
+                    .is_null(USER_TABLE_DELETED_AT_FIELD);
             })
             .fetch_one_as_field_value()
             .await
@@ -78,7 +106,8 @@ impl UserRepository {
         record: impl IntoColumnAndValue,
     ) -> Result<UserEntity, anyhow::Error> {
         let column_and_values = record.into_column_value();
-        let id: String = FieldValue::from_ref_option_into(column_and_values.get("id"));
+        let id: String =
+            FieldValue::from_ref_option_into(column_and_values.get(USER_TABLE_ID_FIELD));
         self.manager
             .insert_record(USER_TABLE, column_and_values)
             .await;
