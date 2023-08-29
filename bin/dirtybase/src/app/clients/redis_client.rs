e786acfacd842110ce52231ec9458343b27f7d05
Jibao Mansaray <jibaomansaray@gmail.com>
2023-08-29 14:39:30 +0000

    Dirtybase: Implement redis client

---
+++ bin/dirtybase/src/app/clients/redis_client.rs
@@ -0,0 +1,23 @@
+use crate::app::Config;
+
+// TODO: Handle redis cluster mode
+
+pub async fn make_redis_client(config: &Config) {
+    if busybody::helpers::service_container()
+        .get::<redis::Client>()
+        .is_none()
+    {
+        let connection = config.redis_connection();
+
+        let client = redis::Client::open(connection.as_ref()).unwrap();
+        busybody::helpers::service_container().set(client);
+    }
+}
+
+pub async fn get_redis_client() -> redis::aio::Connection {
+    if let Some(client) = busybody::helpers::service_container().get::<redis::Client>() {
+        return client.get_async_connection().await.unwrap();
+    } else {
+        panic!("Could not get redis client");
+    }
+}
