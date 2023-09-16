3e5a2f61db4fa82bda88ed28895b072d6099a285
Jibao Mansaray <jibaomansaray@gmail.com>
2023-09-16 12:17:33 +0000

    Remove serde::Deserialize

--- bin/dirtybase/src/app/model/dirtybase_user/dtos/out_logged_in_user_dto.rs
+++ bin/dirtybase/src/app/model/dirtybase_user/dtos/out_logged_in_user_dto.rs
@@ -1,12 +1,13 @@
 use super::out_user_app::UserAppDto;
 use crate::app::model::dirtybase_user::DirtybaseUserEntity;
 use dirtybase_db::{entity::user::dtos::OutUserEntityDto, macros::DirtyTable};
-use dirtybase_db_types::types::StructuredColumnAndValue;
+use dirtybase_db_types::types::{StructuredColumnAndValue, TimestampField};
 
 /// Outgoing DTO when the user successfully logged in
-#[derive(Debug, Default, serde::Serialize, serde::Deserialize, Clone, DirtyTable)]
+#[derive(Debug, Default, serde::Serialize, Clone, DirtyTable)]
 pub struct LoggedInUser {
     pub login_attempt: i64,
+    pub last_login_at: TimestampField,
     pub token: String,
     pub apps: Vec<UserAppDto>,
     pub user: OutUserEntityDto,
@@ -29,6 +30,7 @@ impl From<DirtybaseUserEntity> for LoggedInUser {
             apps: value.apps.into_iter().map(|i| i.into()).collect(),
             user: value.user.into(),
             is_sys_admin: value.is_sys_admin,
+            last_login_at: value.last_login_at,
         }
     }
 }
