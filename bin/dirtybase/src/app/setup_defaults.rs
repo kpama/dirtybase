800bc8e771335219c2142c6df8526247ebc394ad
Jibao Mansaray <jibaomansaray@gmail.com>
2024-03-14 14:56:28 +0000

    Panic when we can't create the default records

--- bin/dirtybase/src/app/setup_defaults.rs
+++ bin/dirtybase/src/app/setup_defaults.rs
@@ -5,5 +5,7 @@ use super::pipeline::setup_system_wide_admin_pipeline::NewSysAdminData;
 pub async fn setup_default_entities() {
     if NewSysAdminData::default().pipeline().await.confirm() {
         log::info!("Default administrator user created");
+    } else {
+        panic!("Could not create default administrator")
     }
 }
