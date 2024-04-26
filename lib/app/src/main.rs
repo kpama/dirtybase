78b8214e0d479e8cfce55ea66b2c456e3d5280a1
Jibao Mansaray <jibaomansaray@gmail.com>
2024-04-26 14:00:00 +0000

    make the `app` crate a lib  Please enter the commit message for your changes. Lines starting

--- lib/app/src/main.rs
+++ lib/app/src/main.rs
@@ -3,7 +3,5 @@ async fn main() -> anyhow::Result<()> {
     pretty_env_logger::init();
     let app_service = dirtybase_app::setup().await?;
 
-    
-
     dirtybase_app::run(app_service.clone()).await
 }
