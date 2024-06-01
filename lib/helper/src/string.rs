001e99cd64a4ccd658eaed886f19a02b18899dc4
Jibao Mansaray <jibaomansaray@gmail.com>
2024-06-01 10:30:25 +1000

    feat: start work on string helpers

---
+++ lib/helper/src/string.rs
@@ -0,0 +1,84 @@
+const DEFAULT_DEMARCATORS: &'static [char] = &[
+    ' ', '_', '-', '/', '\\', '&', '.', ',', '\'', '\"', '/', '+', '=', '\n', '\r', '\t',
+];
+
+pub fn to_pascal_case(subject: &str) -> String {
+    return to_pascal_case_with(subject, DEFAULT_DEMARCATORS.into_iter());
+}
+
+pub fn to_pascal_case_with<D: Iterator<Item = &'static char>>(
+    subject: &str,
+    demarcators: D,
+) -> String {
+    let mut raw = subject.to_string();
+    for demarcator in demarcators {
+        raw = raw
+            .split(demarcator.clone())
+            .map(|p| {
+                let mut s = p.to_string();
+                format!("{}{s}", s.remove(0).to_uppercase())
+            })
+            .collect();
+    }
+
+    print!("stop: {}", &raw);
+    raw
+}
+
+pub fn to_camel_case(subject: &str) -> String {
+    let mut raw = to_pascal_case(subject);
+    format!("{}{raw}", raw.remove(0).to_lowercase())
+}
+
+pub fn to_camel_case_with<D: Iterator<Item = &'static char>>(
+    subject: &str,
+    demarcators: D,
+) -> String {
+    let mut raw = to_pascal_case_with(subject, demarcators);
+    format!("{}{raw}", raw.remove(0).to_lowercase())
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_pascal() {
+        for d in DEFAULT_DEMARCATORS.into_iter() {
+            let subject = format!("foo{d}bar{d}bass");
+            assert_eq!(
+                to_pascal_case(&subject),
+                "FooBarBass",
+                "{} suppose to be {}",
+                subject,
+                "FooBarBass"
+            );
+        }
+    }
+
+    #[test]
+    fn test_pascal_two() {
+        let subject = "my first test";
+        assert_eq!(
+            to_pascal_case(&subject),
+            "MyFirstTest",
+            "{} suppose to be {}",
+            subject,
+            "MyFirstTest"
+        );
+    }
+
+    #[test]
+    fn test_pascal_with() {
+        let subject = format!("fookbarkbass");
+        let d = &['k'];
+        assert_eq!(
+            to_pascal_case_with(&subject, d.into_iter()),
+            "FooBarBass",
+            "{} suppose to be {}",
+            subject,
+            "FooBarBass"
+        );
+    }
+}
