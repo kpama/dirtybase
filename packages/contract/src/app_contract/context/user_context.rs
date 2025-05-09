a4dfe7320c7a9e43766972c611151d79b2e98e92
Jibao Mansaray <jibaomansaray@gmail.com>
2025-05-09 12:26:49 +0000

    use the auth user status instead

--- packages/contract/src/app_contract/context/user_context.rs
+++ packages/contract/src/app_contract/context/user_context.rs
@@ -1,18 +1,18 @@
-use crate::{db_contract::types::ArcUuid7, user::status::UserStatus};
+use crate::{auth_contract::AuthUserStatus, db_contract::types::ArcUuid7};
 
 pub const GLOBAL_USER_CONTEXT_ID: &str = "0194d471-0c6d-75f2-b234-03343345edbc";
 
 #[derive(Debug, Clone, Default, serde::Serialize, serde::Deserialize)]
 pub struct UserContext {
     id: ArcUuid7,
-    status: UserStatus,
+    status: AuthUserStatus,
 }
 
 impl UserContext {
     pub fn make_global() -> Self {
         Self {
             id: ArcUuid7::try_from(GLOBAL_USER_CONTEXT_ID).unwrap(),
-            status: UserStatus::Active,
+            status: AuthUserStatus::Active,
         }
     }
 
@@ -20,11 +20,11 @@ impl UserContext {
         self.id.clone()
     }
 
-    pub fn status(&self) -> UserStatus {
+    pub fn status(&self) -> AuthUserStatus {
         self.status.clone()
     }
 
-    pub fn status_ref(&self) -> &UserStatus {
+    pub fn status_ref(&self) -> &AuthUserStatus {
         &self.status
     }
 
