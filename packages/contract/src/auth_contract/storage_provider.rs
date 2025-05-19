61b57447c26db64c956f4f1c90434a456c8d7d37
Daniel Newman <contact@lifeofdan.me>
2025-05-19 12:02:40 -0400

    Implement exists_by_username trait

--- packages/contract/src/auth_contract/storage_provider.rs
+++ packages/contract/src/auth_contract/storage_provider.rs
@@ -7,6 +7,7 @@ use super::{AuthUser, AuthUserPayload};
 #[async_trait::async_trait]
 pub trait AuthUserStorage: Send + Sync {
     async fn store(&self, payload: AuthUserPayload) -> Result<AuthUser, anyhow::Error>;
+    async fn exists_by_username(&self, username: &str) -> bool;
     async fn find_by_id(&self, id: ArcUuid7) -> Result<Option<AuthUser>, anyhow::Error>;
     async fn find_by_username(&self, username: &str) -> Result<Option<AuthUser>, anyhow::Error>;
     async fn find_by_email_hash(&self, hash: &str) -> Result<Option<AuthUser>, anyhow::Error>;
@@ -36,6 +37,9 @@ impl Deref for AuthUserStorageProvider {
 impl AuthUserStorage for AuthUserStorageProvider {
     async fn store(&self, payload: AuthUserPayload) -> Result<AuthUser, anyhow::Error> {
         self.0.store(payload).await
+    }
+    async fn exists_by_username(&self, username: &str) -> bool {
+        self.0.exists_by_username(username).await
     }
     async fn find_by_id(&self, id: ArcUuid7) -> Result<Option<AuthUser>, anyhow::Error> {
         self.0.find_by_id(id).await
