1ccbdb8c361a346c1b2d93cf2f10db655de52c31
Jibao Mansaray <jibaomansaray@gmail.com>
2025-08-07 23:24:04 +0000

    chore(helper): Add various base64 helpers

--- packages/helper/src/base64.rs
+++ packages/helper/src/base64.rs
@@ -5,5 +5,33 @@ pub fn encode(input: &[u8]) -> String {
 }
 
 pub fn decode(input: &str) -> Result<Vec<u8>, base64ct::Error> {
-    base64ct::Base64::decode_vec(input)
+    let mut result = base64ct::Base64::decode_vec(input);
+    if result.is_ok() {
+        return result;
+    }
+
+    result = url_decode(input);
+    if result.is_ok() {
+        return result;
+    }
+
+    result = url_decode_unpadded(input);
+
+    return result;
+}
+
+pub fn url_encode(input: &[u8]) -> String {
+    base64ct::Base64Url::encode_string(input)
+}
+
+pub fn url_decode(input: &str) -> Result<Vec<u8>, base64ct::Error> {
+    base64ct::Base64Url::decode_vec(input)
+}
+
+pub fn url_encode_unpadded(input: &[u8]) -> String {
+    base64ct::Base64UrlUnpadded::encode_string(input)
+}
+
+pub fn url_decode_unpadded(input: &str) -> Result<Vec<u8>, base64ct::Error> {
+    base64ct::Base64UrlUnpadded::decode_vec(input)
 }
