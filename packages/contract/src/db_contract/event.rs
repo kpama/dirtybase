4fa7d120538e71087af0a99712ef73c4b84723fa
Jibao Mansaray <jibaomansaray@gmail.com>
2025-10-10 06:19:02 +0000

    feat(shared_types): create a crate for shared types

--- packages/contract/src/db_contract/event.rs
+++
@@ -1,5 +0,0 @@
-mod schema_wrote_event;
-mod user_created_event;
-
-pub use schema_wrote_event::SchemeWroteEvent;
-pub use user_created_event::UserCreatedEvent;
--- packages/contract/src/db_contract/types/arc_uuid.rs
+++
@@ -1,5 +0,0 @@
-mod arc_uuid_v4;
-mod arc_uuid_v7;
-
-pub use arc_uuid_v4::*;
-pub use arc_uuid_v7::*;
