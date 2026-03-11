cf125f561be190aae84571079352f6ef06dbee07
Jibao Mansaray <jibaomansaray@gmail.com>
2026-03-11 13:24:52 +0000

    remove reference to 'auth'

--- packages/auth/src/helpers.rs
+++ packages/auth/src/helpers.rs
@@ -1,9 +1,4 @@
-use dirtybase_contract::{
-    auth_contract::{AuthUserStorageProvider, StorageResolver, storage2::PermStorageProvider},
-    prelude::Context,
-};
-
-use crate::AuthExtension;
+use dirtybase_contract::{auth_contract::storage2::PermStorageProvider, prelude::Context};
 
 /// Resolves and return an instance of the storage provider
 pub async fn get_auth_storage(ctx: Context) -> Result<PermStorageProvider, anyhow::Error> {
