df2274326f4b918a3faed01c95110a9098d5140d
Jibao Mansaray <jibaomansaray@gmail.com>
2026-09-14 00:31:25 +1000

    test(config_contract): Make sure test use the same "dev" value

--- contract/src/config_contract/dirtybase_config.rs
+++ contract/src/config_contract/dirtybase_config.rs
@@ -274,7 +274,7 @@ mod test {
     fn test_load_from_dir() {
         let tmp_dir = env::temp_dir();
         let dir = tmp_dir.join("dirty_config");
-        let content = "DTY_APP_NAME=\"My Temp App\" \nDTY_APP_ENV=\"prod\" \n";
+        let content = "DTY_APP_NAME=\"My Temp App\" \nDTY_APP_ENV=\"dev\" \n";
 
         _ = std::fs::create_dir(dir.clone());
 
@@ -282,7 +282,7 @@ mod test {
             Ok(_) => {
                 let config = DirtyConfig::new_at_dir(env::temp_dir().join("dirty_config"));
                 assert_eq!(config.app_name(), "My Temp App");
-                assert_eq!(config.current_env(), &CurrentEnvironment::Production);
+                assert_eq!(config.current_env(), &CurrentEnvironment::Development);
             }
             Err(e) => panic!("could not write temp file: {}", e),
         }
